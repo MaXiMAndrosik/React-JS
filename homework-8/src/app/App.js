9a1cbf402b9bae2072d1936f5307ed8ad1b521c6
Maxim <androsikmaksim@gmail.com>
2025-04-25 01:02:44 +0300

    update

--- homework-8/src/app/App.js
+++ homework-8/src/app/App.js
@@ -1,14 +1,15 @@
 import { StoreProvider } from "./providers/StoreProvider";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
+import ScrollToTop from "./providers/ScrollProvider";
 // import { Notifications } from "@/widgets/Notifications";
 
 function App() {
     return (
         <BrowserRouter>
             <StoreProvider>
+                <ScrollToTop />
                 <AppRouter />
-                {/* <Notifications /> */}
             </StoreProvider>
         </BrowserRouter>
     );
